65a52ed9d2693e49a1a941dc1c09e470de394b66
m-ichelleliu <michelleliu@princeton.edu>
2022-07-07 01:20:20 -0400

    Add check for vertical wins

--- script.js
+++ script.js
@@ -8,6 +8,13 @@ const cols = 7;
 let board = [];
 for (let i = 0; i < cols; i++) {
     board.push([]);
+    for (let j = 0; j < rows; j++) {
+        board[i].push("empty");
+    }
+}
+let colHeights = [];
+for (let i = 0; i < cols; i++) {
+    colHeights.push(0);
 }
 
 console.log(document.getElementsByClassName("board"));
@@ -37,19 +44,29 @@ for (let i = 1; i <= cols; i++) {
 function dropPiece(column) {
     console.log(`dropped in ${column}`);
     console.log(board);
-    board[column-1].push(turn);
+    board[column-1][colHeights[column-1]] = turn;
     // CHECK FOR OUT OF BOUNDS BTW
-    const slotIndex = column + cols * (rows - board[column - 1].length);
+    const slotIndex = column + cols * (rows - colHeights[column - 1] - 1);
     const slot = document.querySelector(`.slot:nth-child(${slotIndex})`);
     const img = slot.firstElementChild;
     img.setAttribute("src", `assets/${turn}.png`);
     if (turn == "p1") turn = "p2";
     else turn = "p1";
-    checkForWin();
+    colHeights[column-1]++;
+    checkForWin("p1");
+    checkForWin("p2");
 }
 
-function checkForWin() {
-
+function checkForWin(player) {
+    // Verticals
+    for (let i = 0; i < cols; i++) {
+        for (let j = 0; j < rows - 3; j++) {
+            if (board[i][j] === player && board[i][j+1] === player &&
+                board[i][j+2] === player && board[i][j+3] === player){
+                    console.log(player + "wins!");
+            }
+        }
+    }
 }
 
 
