459ec33e43daa9a233d7a2bec7e55ca06ee227c8
m-ichelleliu <michelleliu@princeton.edu>
2022-07-11 10:41:15 -0400

    Add menu, form, andsomewhat responsive layout

--- script.js
+++ script.js
@@ -1,44 +1,72 @@
 
 let turn = "p1";
+const sizeform = document.getElementById("sizeform");
+let rows = sizeform.elements['rows'].value;
+let cols = sizeform.elements['cols'].value;
+let winCondition = sizeform.elements['wincondition'].value; /* 4 by default */
+let board = [];
+let colHeights = [];
+generateBoard();
+
+sizeform.addEventListener('submit', (e) => {
+    e.preventDefault();
+    /* Validation not working at the moment */
+    if (sizeform.elements['wincondition'].value > sizeform.elements['rows'].value && sizeform.elements['wincondition'].value > sizeform.elements['cols'].value) {
+        sizeform.appendChild('<p>Bad game rules; impossible win condition</p>');
+        console.log('bruh');
+        return;
+    }
+    generateBoard();
+}
+)
+
+document.getElementById("reset").addEventListener("click", () => {
+    generateBoard();
+}
+)
+
 
 // Generate board
 
-const rows = 6;
-const cols = 7;
-let board = [];
-for (let i = 0; i < cols; i++) {
-    board.push([]);
-    for (let j = 0; j < rows; j++) {
-        board[i].push("empty");
+function generateBoard() {
+    rows = sizeform.elements['rows'].value;
+    cols = sizeform.elements['cols'].value;
+    board = [];
+    for (let i = 0; i < cols; i++) {
+        board.push([]);
+        for (let j = 0; j < rows; j++) {
+            board[i].push("empty");
+        }
     }
-}
-let colHeights = [];
-for (let i = 0; i < cols; i++) {
-    colHeights.push(0);
-}
+    colHeights = [];
+    for (let i = 0; i < cols; i++) {
+        colHeights.push(0);
+    }
+    
+    let boardElement = document.getElementsByClassName("board")[0];
+    boardElement.innerHTML = "";
+    boardElement.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
+    boardElement.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
+    for (let i = 0; i < rows; i++) {
+        for (let j = 0; j < cols; j++) {
+            boardElement.insertAdjacentHTML("afterbegin",
+                `<div class='slot'> 
+                 <img src='assets/empty.png'></img>
+                </div>`
+            );
+        }
+    }
 
-console.log(document.getElementsByClassName("board"));
-let boardElement = document.getElementsByClassName("board")[0];
-boardElement.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
-boardElement.style.gridTemplateRows = `repeat(${rows}, 1fr)`;
-for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < cols; j++) {
-        boardElement.insertAdjacentHTML("afterbegin",
-            `<div class='slot'> 
-             <img src='assets/empty.png'></img>
-            </div>`
-        );
+    // Add event listeners to each column
+    for (let i = 1; i <= cols; i++) {
+        let column = document.querySelectorAll(`.board .slot:nth-child(${cols}n+${i})`);
+        for (const slot of column) {
+            slot.addEventListener("click", () => dropPiece(i));
+            slot.addEventListener("mouseover", () => animateHover(i));
+            slot.addEventListener("mouseleave", () => restoreSlot(i));
+        }
     }
-}
 
-// Add event listeners to each column
-for (let i = 1; i <= cols; i++) {
-    let column = document.querySelectorAll(`.board .slot:nth-child(${cols}n+${i})`);
-    for (const slot of column) {
-        slot.addEventListener("click", () => dropPiece(i));
-        slot.addEventListener("mouseover", () => animateHover(i));
-        slot.addEventListener("mouseleave", () => restoreSlot(i));
-    }
 }
 
 // Show piece placement preview on hover
@@ -53,7 +81,7 @@ function lowestEmptySlot(column) {
 function animateHover(column) {
     img = lowestEmptySlot(column);
     img.setAttribute("src", `assets/${turn}.png`);
-    img.style.opacity = 0.65;
+    img.style.opacity = 0.30;
 }
 
 function restoreSlot(column) {
@@ -73,16 +101,16 @@ function dropPiece(column) {
     img.setAttribute("src", `assets/${turn}.png`);
     img.style.opacity = 1;
     colHeights[column-1]++;
-    if (checkForWin(turn)) console.log(turn + " has won");
+    if (checkForWin(turn)) {
+        document.querySelector(".form-wrapper").insertAdjacentHTML(
+            "beforeend",
+            `<p>${turn} has won! Click reset to play again</p>`
+        )
+        // Should now freeze board so as to avoid clogging up the html...
+    }
     if (turn == "p1") turn = "p2";
     else turn = "p1";
     img.dispatchEvent(new MouseEvent("mouseover", {"bubbles": true}));
-}
-
-const winCondition = 4; /* 4 by default */
-
-if (winCondition > rows && winCondition > cols) {
-    throw 'Bad game rules; impossible win condition';
 }
 
 function checkForWin(player) {
