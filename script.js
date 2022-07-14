7aae2fbd941e27efe920444b2a23101f68b09619
m-ichelleliu <michelleliu@princeton.edu>
2022-07-14 10:26:01 -0400

    Add drop animations; animateHover is still glitchy

--- script.js
+++ script.js
@@ -55,10 +55,44 @@ class GameBoard {
     // Game Behavior
     dropPiece(column) {
         console.log(`dropped in ${column}`);
+
+        // Do nothing if column is filled
+        if (this.colHeights[column - 1] == this.rows) return;
+
+        // Update board state
         this.board[column-1][this.colHeights[column-1]] = this.turn;
-        // CHECK FOR OUT OF BOUNDS BTW
-        this.updateLowestEmptySlot(column, `assets/${this.turn}.png`, 1);
-        this.colHeights[column-1]++;
+        console.log(this.board);
+        
+        // Animate dropping
+        // let colwidth = getComputedStyle(this.lowestEmptySlotImg(column)).getPropertyValue("width");
+        // let colpos = getComputedStyle(this.lowestEmptySlotImg(column)).getPropertyValue("left");
+        // console.log(colwidth + " " + colpos);
+        
+        // const fallingPiece = document.createElement("img");
+        // fallingPiece.innerHTML = ""
+
+        // console.log(getComputedStyle(this.lowestEmptySlotImg(column)));
+
+        // Simpler animate dropping
+        let piece_y = this.rows;
+        let currentTurn = this.turn;
+        let timer = setInterval(() => {
+            if (piece_y == this.colHeights[column - 1]) {
+                clearInterval(timer);
+                this.updateLowestEmptySlot(column, `assets/${currentTurn}.png`, 1);
+                this.colHeights[column-1]++;
+                if (this.lowestEmptySlotImg(column) != null)
+                    this.lowestEmptySlotImg(column).dispatchEvent(new MouseEvent("mouseover", {"bubbles": true}));
+            }
+            else {
+                if (piece_y != this.rows)
+                    this.imageAtPos(this.rows - piece_y, column + 1).setAttribute("src", `assets/empty.png`);
+                this.imageAtPos(this.rows - piece_y + 1, column + 1).setAttribute("src", `assets/${currentTurn}.png`);
+                piece_y -= 1;
+            }
+        }, 80);
+
+        // Check if game has ended
         if (this.checkForWin(this.turn)) {
             document.querySelector(".form-wrapper").insertAdjacentHTML(
                 "beforeend",
@@ -83,12 +117,14 @@ class GameBoard {
             return;
             // boardElement.style.removeProperty("cursor"); // Should remove by slot instead of whole board. oops
         }
+
+        // If game has not ended, prepare for the next turn
         if (this.turn == "p1") this.turn = "p2";
         else this.turn = "p1";
         if (this.lowestEmptySlotImg(column) != null)
             this.lowestEmptySlotImg(column).dispatchEvent(new MouseEvent("mouseover", {"bubbles": true}));
     }
-    
+
     checkForWin(player) {
         // This could probably be condensed but I can't yet think of a way to condense it that doesn't sacrifice readability
 
@@ -168,6 +204,18 @@ class GameBoard {
 
     restoreSlot(column) {
         this.updateLowestEmptySlot(column, `assets/empty.png`, 1);
+    }
+
+    // Helper functions for animation
+
+    // 
+    imageAtPos(row, col) { 
+        const slotIndex = (col - 1) + this.cols * (row - 1);
+        console.log(slotIndex);
+        const slot = document.querySelector(`.slot:nth-child(${slotIndex})`);
+        if (slot == null) return null;
+        const img = slot.firstElementChild;
+        return img;
     }
 }
 
